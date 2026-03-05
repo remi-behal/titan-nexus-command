044db05fe60ae801ad2ee0d25472c2c0aa32d3fe
Remi Behal <remibehal@gmail.com>
2026-03-05 14:00:08 +0000

    chore: create VisualStats constants for client side

---
+++ client/src/constants/VisualStats.js
@@ -0,0 +1,9 @@
+export const VISUAL_STATS = {
+    SUPER_NODE: {
+        AURA_PULSE_SPEED: 200,      // Divisor for Date.now()
+        AURA_PULSE_MAGNITUDE: 5,    // Max pixel expansion
+        AURA_RADIUS_SCALE: 2,       // Multiplier for base radius
+        AURA_COLOR: 'rgba(191, 0, 255, 0.3)',
+        AURA_DEFAULT_RADIUS: 20
+    }
+};
